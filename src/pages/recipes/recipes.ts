88324df24b88e967b00b56e92c1098a962181afd
Vivek Singh <vivek.singh1@kelltontech.com>
2018-03-16 12:10:01 +0530

    final app using firebase

--- src/pages/recipes/recipes.ts
+++ src/pages/recipes/recipes.ts
@@ -44,7 +44,9 @@ export class RecipesPage {
     const popover = this.popoverCtrl.create(SlOptionsPage)
     popover.present({ ev: event });
     popover.onDidDismiss(data => {
-
+      if(!data){
+        return;
+      }
       if (data.action == 'load') {
         loading.present();
         this.authService.getActiveUser().getToken()
